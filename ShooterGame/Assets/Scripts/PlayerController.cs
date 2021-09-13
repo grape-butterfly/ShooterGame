e1251e88065a6c1c75f806a2df6a3befa6481897
grape-butterfly <emb4130@utulsa.edu>
2021-09-13 16:41:32 -0500

    Player Object Created
    Setup player object
    Started PlayerController script

---
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+/***
+ * Created By: Rain Baldridge
+ * Date Created: Sept 13, 2021
+ * 
+ * Last Edited By:
+ * Last Updated: Sept 13, 2021
+ * 
+ * Description: Player control movements.
+ * 
+ ***/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
